1dc38a52e74044e3756673fa48ebc7217461a46c
Vikram Sharma <vsharma8363@gmail.com>
2018-06-05 19:34:52 -0700

    Lane detection complete

--- LaneDetection.py
+++ LaneDetection.py
@@ -17,6 +17,12 @@ def filter_region(image, vertices):
         cv2.fillPoly(mask, vertices, (255,) * mask.shape[2])  # in case, the input image has a channel dimension
     return cv2.bitwise_and(image, mask)
 
+def midpoint(p1, p2):
+    x = (p1[0]+p2[0])/2
+    y = (p1[1]+p2[1])/2
+    p = [x, y]
+    return p
+
 cap = cv2.VideoCapture('test.mp4')
 
 while (True):
@@ -28,82 +34,87 @@ while (True):
     height = np.size(frame, 0)
     width = np.size(frame, 1)
 
-    #frame = cv2.blur(frame, (2, 2))
-
     edges = cv2.Canny(frame, 100, 200)
 
     rows, cols = edges.shape[:2]
     bottom_left = [cols * 0.1, rows * 0.9]
-    top_left = [cols * 0.4, rows * 0.6]
+    top_left = [cols * 0.25, rows * 0.55]
     bottom_right = [cols * 0.9, rows * 0.9]
-    top_right = [cols * 0.6, rows * 0.6]
+    top_right = [cols * 0.75, rows * 0.55]
+
     # the vertices are an array of polygons (i.e array of arrays) and the data type must be integer
     vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
     cropped_edges = filter_region(edges, vertices)
 
-    hough_lines = cv2.HoughLinesP(cropped_edges, rho=1, theta=np.pi / 180, threshold=20, minLineLength=50,maxLineGap=50)
+    #INSERT CODE HERE
+    # Find all lines in the image
+    hough_lines = cv2.HoughLinesP(cropped_edges, rho=1, theta=np.pi / 180, threshold=20, minLineLength=50, maxLineGap=50)
 
-    if hough_lines is None:
-        continue
-
-    list_of_lines = list(hough_lines)
+    if hough_lines is not None:
+        list_of_lines = list(hough_lines)
 
     totalM = 0
-    m = 3
+    m = 0
 
-    longestLeftLine = [0,0,0,0]
-    longestRightLine = [0,0,0,0]
+    xCenter = width / 2
+    yCenter = height*0.75
 
-    xCenter = width/2
+    leftLane = [-1,-1,-1,-1]
+    rightLane = [-1,-1,-1,-1]
 
     for i in list_of_lines:
         arr = i[0]
-        lineThickness = 1
         x1 = arr[0]
         y1 = arr[1]
         x2 = arr[2]
         y2 = arr[3]
+        #Check if the line you are looking at has a slope near 0
+        isNotFlatStraight = True
+        if abs(float(x2) - float(x1)) < 50:
+            isNotFlatStraight = False
+            #if lane line is on the left side
+        if (x1 < xCenter and x2 < xCenter) and isNotFlatStraight:
+            if calculateDistance(leftLane[0], leftLane[1], leftLane[2], leftLane[3]) < calculateDistance(x1, y1, x2, y2):
+                leftLane = [x1, y1, x2, y2]
+            # if lane line is on the right side
+        elif (x1 > xCenter and x2 > xCenter) and isNotFlatStraight:
+            if calculateDistance(rightLane[0], rightLane[1], rightLane[2], rightLane[3]) < calculateDistance(x1, y1, x2, y2):
+                rightLane = [x1, y1, x2, y2]
 
-        #on the left
-        if x1 < xCenter:
-            if calculateDistance(x1, y1, x2, y2) > calculateDistance(longestLeftLine[0],longestLeftLine[1],longestLeftLine[2],longestLeftLine[3]):
-                longestLeftLine = [x1, y1, x2, y2]
-        else:
-            if calculateDistance(x1, y1, x2, y2) > calculateDistance(longestRightLine[0],longestRightLine[1],longestRightLine[2],longestRightLine[3]):
-                longestRightLine = [x1, y1, x2, y2]
+    cv2.line(frame, (rightLane[0], rightLane[1]), (rightLane[2], rightLane[3]), (255, 0, 0), 5)
+    cv2.line(frame, (leftLane[0], leftLane[1]), (leftLane[2], leftLane[3]), (255, 0, 0), 5)
 
+    angle = 0
 
+    #check if right lane is valid
+    if rightLane[0] != -1 and (float(rightLane[2]) - float(rightLane[0])) != 0:
+        #right lane is valid
+        totalM += (float(rightLane[1]) - float(rightLane[3])) / (float(rightLane[2]) - float(rightLane[0]))
+        m+=1
+    if leftLane[0] != -1 and (float(leftLane[2]) - float(leftLane[0])) != 0:
+        #right lane is valid
+        totalM += (float(leftLane[1]) - float(leftLane[3])) / (float(leftLane[2]) - float(leftLane[0]))
+        m+=1
 
-    cv2.line(frame, (longestLeftLine[0],longestLeftLine[1]), (longestLeftLine[2],longestLeftLine[3]), (255, 0, 0), 10)
-    cv2.line(frame, (longestRightLine[0],longestRightLine[1]), (longestRightLine[2],longestRightLine[3]), (255, 0, 0), 10)
-
-
-    if (float(longestLeftLine[2]) - float(longestLeftLine[0])) != 0:
-        totalM += (float(longestLeftLine[1]) - float(longestLeftLine[3])) / (float(longestLeftLine[2]) - float(longestLeftLine[0]))
-
-    if (float(longestRightLine[2]) - float(longestRightLine[0])) != 0:
-        totalM += (float(longestRightLine[1]) - float(longestRightLine[3])) / (float(longestRightLine[2]) - float(longestRightLine[0]))
-
-    if calculateDistance(longestLeftLine[0],longestLeftLine[1],longestLeftLine[2],longestLeftLine[3]) == 0 or calculateDistance(longestRightLine[0],longestRightLine[1],longestRightLine[2],longestRightLine[3]) == 0:
-        confidence = 0
-        m = 15
-    else:
-        confidence = 10
-
+    run = 100
     rise = 100
-    run = rise * totalM / m
 
-    angle = atan(run/rise)*57.2958
+    if m != 0:
+        rise = 100
+        run = rise * totalM / m
+        angle = atan(run / rise) * 57.2958
+    else:
+        angle = 0
 
-    #angle --> The degrees to turn in either direction
-    #confidence --> confidence of making the turn at that angle, 0 to 10
+    # angle --> The degrees to turn in either direction
+    # confidence --> confidence of making the turn at that angle, 0 to 10
+
 
     print(angle)
-    print(confidence)
 
     cv2.line(frame, (width / 2, int(height * 0.75)), (int(width / 2 + run), int(height * 0.75 - rise)), (0, 255, 0), 5)
 
-# Display the resulting frame
+    # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
